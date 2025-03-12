54a3a92ad775db7c784190f460ede10e6fb0f719
Ece Yaman <eceymn35@gmail.com>
2025-03-12 17:36:37 +0300

    task completed

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    String address;
+    String phoneNumber;
+    String email;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, int age, String address, String phoneNumber, String email) {
+        this(firstName, lastName, age);
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        return age >= 13 && age <= 19;
+    }
+}
